4419507bd867cc644119d445becb5d5a8adb65f3
yangywtrue <1141583038@qq.com>
2017-05-25 16:46:54 +0800

    添加字符串嵌入值

---
+++ 6.StringInterpolation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "ZhangSan";
+            string doing = "running";
+            Console.WriteLine($"{name} is {doing}");
+            Console.ReadKey();
+        }
+    }
+}
